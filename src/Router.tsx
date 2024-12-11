9bbd3181aaf95989fd619a3a50f9e6d9c3980895
WQYeo <yeo_dev@protonmail.com>
2024-12-11 17:41:37 +0800

    Fix Github Pages deploy with router

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,7 +3,7 @@ import { HomePage } from './pages/Home.page';
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: '/Vite-Mantine-Deploy-Github-Pages',
     element: <HomePage />,
   },
 ]);
